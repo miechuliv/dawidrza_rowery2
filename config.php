91248ef8fe796aba35b9ed40330afa77d2f78dbb
Mieszko Malawski <miechuliv@tlen.pl>
2014-09-24 12:40:29 +0200

    test2

--- config.php
+++ config.php
@@ -1,28 +1,28 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://demo.stronazen.pl/nasz/');
-define('HTTP_IMAGE', 'http://demo.stronazen.pl/nasz/image/');
-define('HTTP_ADMIN', 'http://demo.stronazen.pl/nasz/admin/');
+define('HTTP_SERVER', 'http://demo.stronazen.pl/rowery2/');
+define('HTTP_IMAGE', 'http://demo.stronazen.pl/rowery2/image/');
+define('HTTP_ADMIN', 'http://demo.stronazen.pl/rowery2/admin/');
 
-define('HTTP_SUB', 'nasz/');
+define('HTTP_SUB', 'rowery2/');
 
 
 
 // HTTPS
-define('HTTPS_SERVER', 'http://demo.stronazen.pl/nasz/');
-define('HTTPS_IMAGE', 'http://demo.stronazen.pl/nasz/image/');
+define('HTTPS_SERVER', 'http://demo.stronazen.pl/rowery2/');
+define('HTTPS_IMAGE', 'http://demo.stronazen.pl/rowery2/image/');
 
 // DIR
-define('DIR_APPLICATION', '/home/demostrona/domains/demo.stronazen.pl/public_html/nasz/catalog/');
-define('DIR_SYSTEM', '/home/demostrona/domains/demo.stronazen.pl/public_html/nasz/system/');
-define('DIR_DATABASE', '/home/demostrona/domains/demo.stronazen.pl/public_html/nasz/system/database/');
-define('DIR_LANGUAGE', '/home/demostrona/domains/demo.stronazen.pl/public_html/nasz/catalog/language/');
-define('DIR_TEMPLATE', '/home/demostrona/domains/demo.stronazen.pl/public_html/nasz/catalog/view/theme/');
-define('DIR_CONFIG', '/home/demostrona/domains/demo.stronazen.pl/public_html/nasz/system/config/');
-define('DIR_IMAGE', '/home/demostrona/domains/demo.stronazen.pl/public_html/nasz/image/');
-define('DIR_CACHE', '/home/demostrona/domains/demo.stronazen.pl/public_html/nasz/system/cache/');
-define('DIR_DOWNLOAD', '/home/demostrona/domains/demo.stronazen.pl/public_html/nasz/download/');
-define('DIR_LOGS', '/home/demostrona/domains/demo.stronazen.pl/public_html/nasz/system/logs/');
+define('DIR_APPLICATION', '/home/demostrona/domains/demo.stronazen.pl/public_html/rowery2/catalog/');
+define('DIR_SYSTEM', '/home/demostrona/domains/demo.stronazen.pl/public_html/rowery2/system/');
+define('DIR_DATABASE', '/home/demostrona/domains/demo.stronazen.pl/public_html/rowery2/system/database/');
+define('DIR_LANGUAGE', '/home/demostrona/domains/demo.stronazen.pl/public_html/rowery2/catalog/language/');
+define('DIR_TEMPLATE', '/home/demostrona/domains/demo.stronazen.pl/public_html/rowery2/catalog/view/theme/');
+define('DIR_CONFIG', '/home/demostrona/domains/demo.stronazen.pl/public_html/rowery2/system/config/');
+define('DIR_IMAGE', '/home/demostrona/domains/demo.stronazen.pl/public_html/rowery2/image/');
+define('DIR_CACHE', '/home/demostrona/domains/demo.stronazen.pl/public_html/rowery2/system/cache/');
+define('DIR_DOWNLOAD', '/home/demostrona/domains/demo.stronazen.pl/public_html/rowery2/download/');
+define('DIR_LOGS', '/home/demostrona/domains/demo.stronazen.pl/public_html/rowery2/system/logs/');
 
 define('ENV', 'TESTING');
 
